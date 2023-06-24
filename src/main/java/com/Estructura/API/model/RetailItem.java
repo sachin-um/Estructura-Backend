8dc3dde1c2811e44eedce75d5cbf0aa096b365ab
Sachin Umayangana <sachinumayangana@gmail.com>
2023-06-24 17:45:28 +0530

    change the workflow

---
+++ src/main/java/com/Estructura/API/model/RetailItem.java
@@ -0,0 +1,19 @@
+package com.Estructura.API.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RetailItem {
+
+    @Id
+    private String id;
+}
