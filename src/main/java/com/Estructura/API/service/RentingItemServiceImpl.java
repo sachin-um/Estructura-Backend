916bbb612aae22d0a106e1bc63d765009834177b
Sachin Umayangana <sachinumayangana@gmail.com>
2023-08-05 20:09:44 +0530

    Adding getall and resolve an error

--- src/main/java/com/Estructura/API/service/RentingItemServiceImpl.java
+++ src/main/java/com/Estructura/API/service/RentingItemServiceImpl.java
@@ -54,6 +54,16 @@ public class RentingItemServiceImpl implements RentingItemService{
         return response;
     }
 
+    @Override
+    public ResponseEntity<List<RentingItem>> getAllItem() {
+        List<RentingItem> rentingItems=rentingItemRepository.findAll();
+        if (!rentingItems.isEmpty()){
+            return ResponseEntity.ok(rentingItems);
+        }
+        else {
+            return ResponseEntity.noContent().build();
+        }
+    }
 
 
     @Override
